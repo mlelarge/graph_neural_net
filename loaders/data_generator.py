582d016dd8149e81cbcf83e4f1058c88b8cf5e82
Waïss Azizian <waissfowl@gmail.com>
2020-02-28 18:27:06 +0100

    Reorganize loaders/data_generator.py
    Mainly, remove numpy dependency by using equivalent torch funcs

--- loaders/data_generator.py
+++ loaders/data_generator.py
@@ -1,11 +1,38 @@
-import numpy as np
 import os
 import networkx
 import torch
-import torch.nn as nn
 import torch.utils
-import torch.nn.functional as F
 from toolbox import utils
+
+def generate_erdos_renyi_netx(p, N):
+    """ Generate random Erdos Renyi graph """
+    g = networkx.erdos_renyi_graph(N, p)
+    W = networkx.adjacency_matrix(g).todense()
+    return torch.as_tensor(W, dtype=torch.float)
+
+def generate_barabasi_albert_netx(p, N):
+    """ Generate random Barabasi Albert graph """
+    m = int(p*(N -1)/2)
+    g = networkx.barabasi_albert_graph(N, m)
+    W = networkx.adjacency_matrix(g).todense()
+    return torch.as_tensor(W, dtype=torch.float)
+
+def generate_regular_graph_netx(p, N):
+    """ Generate random regular graph """
+    d = p * N
+    d = int(d)
+    g = networkx.random_regular_graph(d, N)
+    W = networkx.adjacency_matrix(g).todense()
+    return torch.as_tensor(W, dtype=torch.float)
+
+def adjacency_matrix_to_tensor_representation(W):
+    """ Create a tensor B[:,:,1] = W and B[i,i,0] = deg(i)"""
+    degrees = W.sum(1)
+    B = torch.zeros((len(W), len(W), 2))
+    B[:, :, 1] = W
+    indices = torch.arange(len(W))
+    B[indices, indices, 0] = degrees
+    return B
 
 class Generator(torch.utils.data.Dataset):
     """
@@ -32,66 +59,36 @@ class Generator(torch.utils.data.Dataset):
                                          subfolder_name)
         utils.check_dir(self.path_dataset)
 
-    def ErdosRenyi_netx(self, p, N):
-        g = networkx.erdos_renyi_graph(N, p)
-        W = networkx.adjacency_matrix(g).todense().astype(float)
-        W = np.array(W)
-        return W
-
-    def BarabasiAlbert_netx(self, p, N):
-        m = int(p*(N -1)/2)
-        g = networkx.barabasi_albert_graph(N, m)
-        W = networkx.adjacency_matrix(g).todense().astype(float)
-        W = np.array(W)
-        return W
-
-    def RegularGraph_netx(self, p, N):
-        """ Generate random regular graph """
-        d = p * N
-        d = int(d)
-        g = networkx.random_regular_graph(d, N)
-        W = networkx.adjacency_matrix(g).todense().astype(float)
-        W = np.array(W)
-        return W
-
-    def adjacency_matrix_to_tensor_representation(self, W):
-        """ Create a tensor B[:,:,1] = W and B[i,i,0] = deg(i)"""
-        degrees = W.sum(1)
-        B = np.zeros((self.n_vertices, self.n_vertices, 2))
-        B[:, :, 1] = W
-        indices = np.arange(self.n_vertices)
-        B[indices, indices, 0] = degrees
-        return B
 
     def compute_example(self):
         """
         Compute pairs (Adjacency, noisy Adjacency)
         """
         if self.generative_model == 'ErdosRenyi':
-            W = self.ErdosRenyi_netx(self.edge_density, self.n_vertices)
+            W = generate_erdos_renyi_netx(self.edge_density, self.n_vertices)
         elif self.generative_model == 'Regular':
-            W = self.RegularGraph_netx(self.edge_density, self.n_vertices)
+            W = generate_regular_graph_netx(self.edge_density, self.n_vertices)
         elif self.generative_model == 'BarabasiAlbert':
-            W = self.BarabasiAlbert_netx(self.edge_density, self.n_vertices)
+            W = generate_barabasi_albert_netx(self.edge_density, self.n_vertices)
 
         else:
             raise ValueError('Generative model {} not supported'
                              .format(self.generative_model))
         pe1 = self.noise
-        pe2 = (self.edge_density*self.noise)/(1.0-self.edge_density)
-        noise1 = self.ErdosRenyi_netx(pe1, self.n_vertices)
-        noise2 = self.ErdosRenyi_netx(pe2, self.n_vertices)
+        pe2 = (self.edge_density*self.noise)/(1-self.edge_density)
+        noise1 = generate_erdos_renyi_netx(pe1, self.n_vertices)
+        noise2 = generate_erdos_renyi_netx(pe2, self.n_vertices)
         W_noise = W*(1-noise1) + (1-W)*noise2
-        B = self.adjacency_matrix_to_tensor_representation(W)
-        B_noise = self.adjacency_matrix_to_tensor_representation(W_noise)
+        B = adjacency_matrix_to_tensor_representation(W)
+        B_noise = adjacency_matrix_to_tensor_representation(W_noise)
         return (B, B_noise)
 
     def create_dataset(self):
-        for i in range(self.num_examples):
+        for _ in range(self.num_examples):
             example = self.compute_example()
-            self.data.append(np.asarray(example))
+            self.data.append(example)
 
-    def load_dataset(self, saving=True):
+    def load_dataset(self):
         """
         Look for required dataset in files and create it if
         it does not exist
@@ -100,22 +97,19 @@ class Generator(torch.utils.data.Dataset):
         path = os.path.join(self.path_dataset, filename)
         if os.path.exists(path):
             print('Reading dataset at {}'.format(path))
-            with open(path, 'rb') as f:
-                data = np.load(f, allow_pickle=True)
-                self.data = list(data)
+            data = torch.load(path)
+            self.data = list(data)
         else:
             print('Creating dataset.')
             self.data = []
             self.create_dataset()
             print('Saving datatset at {}'.format(path))
-            with open(path, 'wb') as f:
-                    np.save(f, self.data)
-        return self.data
+            torch.save(self.data, path)
 
-    def __getitem__(self ,i):
+    def __getitem__(self, i):
+        """ Fetch sample at index i """
+        return self.data[i]
 
-        return (torch.Tensor(self.data[i][0]), torch.Tensor(self.data[i][1]))
-    
     def __len__(self):
+        """ Get dataset length """
         return len(self.data)
-
