347b56fe3cd02061b08546f4ec3026e4e7e27464
MauTrib <amaury.triboulin@gmail.com>
2021-03-15 10:14:07 +0100

    Added edge probability visualization

--- toolbox/vision.py
+++ toolbox/vision.py
@@ -1,6 +1,7 @@
 import networkx
 import matplotlib.pyplot as plt
 import os
+import torch
 
 def compare(xs,ys,adj_out,adj_target,save_out=True,path='pics/',name='graph'):
     N = len(xs)
@@ -55,4 +56,39 @@ def show_tour(xs,ys,adj,path='pics/',name='graph'):
     plt.show()
     fname = os.path.join(path,name)
     plt.savefig(fname+".png", format="PNG")
+    plt.close()
+
+def show_probas(probs,solution=None,path='pics/',name='graph'):
+    """
+    adj=adjacency matrix, should be of size (n,n) and assumed symmetric, with probabilities on edges
+    """
+    if solution is None:
+        solution = torch.zeros_like(probs)
+        sol_nodes = []
+    else:
+        sol_nodes = torch.where(torch.sum(solution,dim=1))[0] #Sum over rows, then keep the ones that have a degree
+    n,_ = probs.shape
+    g = networkx.empty_graph(n)
+    black = torch.ones((3))
+    maxi = torch.max(probs)
+    probs = probs/maxi
+    for i in range(1,n):
+        for j in range(i,n):
+            if solution[i,j]:
+                g.add_edge(i,j,color = torch.tensor([1,0,0]), weight = torch.exp(-1/probs[i,j]))
+            elif probs[i,j]:
+                g.add_edge(i,j,color = (1-probs[i,j])*black,weight = torch.exp(-1/probs[i,j]))
+    largest_component = max(networkx.connected_components(g), key=len)
+    g2 = g.subgraph(largest_component)
+    edges = g.edges()
+    colors = [[g[u][v]['color'][i].item() for i in range(3)] for u,v in edges]
+    node_color = ['red' if i in sol_nodes else 'blue' for i in g2.nodes]
+    #print([min(1e9,1/probs[u,v].item()) for u,v in edges])
+    pos = networkx.spring_layout(g2)#networkx.kamada_kawai_layout(g2,[min(1e9,probs[u,v].item()) for u,v in edges])
+    #alpha = [probs[u,v].item() for u,v in edges]
+    plt.figure()
+    networkx.draw(g2,pos=pos,node_color=node_color,edge_color=colors,node_size=5)
+    plt.show()
+    fname = os.path.join(path,name)
+    plt.savefig(fname+".png", format="PNG",dpi=1000)
     plt.close()
